1ca133ab33c3e9c25b1c1ddea5e0c00f7d1767f8
inesuribeb <inesuribeb@gmail.com>
2024-12-04 16:01:50 +0100

    middlewares seccion Cliente

--- src/routes/clientRouter.js
+++ src/routes/clientRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import clientApiController from "../controllers/client/clientApiController.js"
 import authApiController from "../controllers/auth/authApiController.js"
+import { isWorker, allAllowed, isAdmin, isClient } from "../controllers/middleware/authMiddleware.js";
 
 const router = Router()
 
-router.get('/api/clients', clientApiController.showClients);
-router.get('/api/clients/:id', clientApiController.showClientById);
-router.post('/api/clients/create', clientApiController.createClientAPI);
-router.put('/api/clients/:id/update', clientApiController.updateClientAPI);
-router.delete('/api/clients/:id/delete', clientApiController.removeClientAPI);
+router.get('/api/clients', isWorker, clientApiController.showClients);
+router.get('/api/clients/:id', allAllowed, clientApiController.showClientById);
+router.post('/api/clients/create', isAdmin, clientApiController.createClientAPI);
+router.put('/api/clients/:id/update', isClient, clientApiController.updateClientAPI);
+router.delete('/api/clients/:id/delete', isClient, clientApiController.removeClientAPI);
 
 router.post('/api/clients/login', authApiController.loginClient);
 router.post('/api/clients/register', authApiController.registerClient);
