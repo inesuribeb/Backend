dbaa4a4def5addfc6713ee957a8ecf35778a6663
inesuribeb <inesuribeb@gmail.com>
2024-12-04 16:06:10 +0100

    middlewares seccion reservas

--- src/routes/reservationRouter.js
+++ src/routes/reservationRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import ubpApiController from "../controllers/users_book_packs/ubpApiController.js"
+import { isWorker, isClient } from "../controllers/middleware/authMiddleware.js";
 
 const router = Router()
 
-router.get('/api/reservations', ubpApiController.getAllReservations);
-router.get('/api/reservations/:id', ubpApiController.showReservationById);
-router.post('/api/reservations/create', ubpApiController.createReservationAPI);
-router.put('/api/reservations/:id/cancel', ubpApiController.cancelReservationAPI);
-router.put('/api/reservations/:id/status', ubpApiController.updateReservationStatusAPI);
+router.get('/api/reservations', isWorker, ubpApiController.getAllReservations);
+router.get('/api/reservations/:id', isWorker, ubpApiController.showReservationById);
+router.post('/api/reservations/create', isClient, ubpApiController.createReservationAPI);
+router.put('/api/reservations/:id/cancel', isClient, ubpApiController.cancelReservationAPI);
+router.put('/api/reservations/:id/status', isWorker, ubpApiController.updateReservationStatusAPI);
 
 
 
