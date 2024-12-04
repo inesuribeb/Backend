a9cfde527bc95aa738772238f4332cbda51471d2
inesuribeb <inesuribeb@gmail.com>
2024-12-04 16:04:04 +0100

    middlewares seccion pack

--- src/routes/packRouter.js
+++ src/routes/packRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import packApiController from "../controllers/pack/packApiController.js"
+import { isAdmin } from "../controllers/middleware/authMiddleware.js";
 
 const router = Router()
 
 router.get('/api/packs', packApiController.getAllPacks);
 router.get('/api/packs/:id', packApiController.getPackById);
-router.post('/api/packs/create', packApiController.createPackAPI);
-router.put('/api/packs/:id/deactivate', packApiController.deactivatePackAPI);
+router.post('/api/packs/create', isAdmin, packApiController.createPackAPI);
+router.put('/api/packs/:id/deactivate', isAdmin, packApiController.deactivatePackAPI);
 
 
 export default router
