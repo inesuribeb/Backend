a26ebb65bc55ed8502ea82d52e3dad2361ec091d
inesuribeb <inesuribeb@gmail.com>
2024-12-04 16:09:24 +0100

    middlewares seccion worker

--- src/routes/workerRouter.js
+++ src/routes/workerRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import workerApiController from "../controllers/worker/workerApiController.js"
 import authApiController from "../controllers/auth/authApiController.js"
+import { isWorker, isAdmin } from "../controllers/middleware/authMiddleware.js";
 
 const router = Router()
 
-router.get('/api/workers', workerApiController.getAllWorkers);
-router.get('/api/workers/:id', workerApiController.getWorkerById);
-router.post('/api/workers/create', workerApiController.createWorkerAPI);
-router.put('/api/workers/:id/update', workerApiController.updateWorkerAPI);
-router.delete('/api/workers/:id/delete', workerApiController.deleteWorkerAPI);
+router.get('/api/workers', isAdmin, workerApiController.getAllWorkers);
+router.get('/api/workers/:id', isWorker, workerApiController.getWorkerById);
+router.post('/api/workers/create', isAdmin, workerApiController.createWorkerAPI);
+router.put('/api/workers/:id/update', isWorker, workerApiController.updateWorkerAPI);
+router.delete('/api/workers/:id/delete', isAdmin, workerApiController.deleteWorkerAPI);
 
 router.post('/api/workers/login', authApiController.loginWorker);
 
